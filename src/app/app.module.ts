82490d8d3bf9216d758fde32e2f4d985f7aee903
Mohamed BEN YOUSSEF <moh.benysf@gmail.com>
2019-09-28 16:24:28 +0200

    crud with ang8 and nodejs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductGetComponent } from './product-get/product-get.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductGetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule, SlimLoadingBarModule,ReactiveFormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
